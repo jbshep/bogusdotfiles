73beb024d2bc096e2c60982228dd7529b25dd4ae
jbshep <jbshep@gmail.com>
2021-03-04 10:37:21 -0600

    Fixed remote install bug

--- remote-install.sh
+++ remote-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DOTFILES=(.vimrc .inputrc)
-for f in ${DOTFILES}; do
+for f in ${DOTFILES[@]}; do
     curl https://raw.githubusercontent.com/jbshep/bogusdotfiles/main/${f} >~/${f} 2>/dev/null
 done
 
