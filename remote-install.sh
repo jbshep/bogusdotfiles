9aa25d9bca38118faf664f5676f711f8ac52690c
jbshep <jbshep@gmail.com>
2021-03-04 10:20:05 -0600

    Added remote-install script

---
+++ remote-install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/jbshep/bogusdotfiles/main/.vimrc >~/.vimrc 2>/dev/null
+curl https://raw.githubusercontent.com/jbshep/bogusdotfiles/main/.inputrc >~/.inputrc 2>/dev/null
